5af36190ebda3c2fc67e75c5e4e1456c4c2147f3
Danail Dimitrov <danaildimitrov321@gmail.com>
2020-11-22 01:01:33 +0200

    Adding AddWithZip and BrokenZipper

---
+++ FunctionsAdv.hs
@@ -0,0 +1,26 @@
+main :: IO ()
+main = do
+    addWithZip
+
+    brokenZipper
+
+    brokenZipperWith
+
+
+addWithZip = do
+    let arr1 = [1, 2, 3, 4, 5]
+    let arr2 = [1, 2, 3, 4, 5]
+
+    print $map (\(x, y) -> x + y) (zip arr1 arr2)
+
+brokenZipper = do 
+    let arr1 = [23, -6, 48, 54, 12, -5]
+    let arr2 = [15, -3, 55, 3, -4, 6]
+
+    print $map (\(x, y) -> if x < 0 || y < 0 then 0 else x + y) (zip arr1 arr2)
+
+brokenZipperWith = do
+    let arr1 = [23, -6, 48, 54, 12, -5]
+    let arr2 = [15, -3, 55, 3, -4, 6] 
+
+    print $zipWith (\x y -> if x < 0 || y < 0 then 0 else x + y) arr1 arr2
